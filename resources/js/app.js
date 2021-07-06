48c6ec7019a422ce514b45560647f3f108feed1b
Martab1 <martabernardo86@gmail.com>
2021-07-06 10:49:37 +0200

    done: initialize project

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+/**
+  FRONT OFFICE
+ **/
+
+window.Vue = require('vue');
+
+import App from './App.vue';
+
+const app = new Vue({
+    el: '#root',
+    render: h=>h(App),
+});
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+Route::prefix('admin')
+       ->namespace('Admin')
+       ->middleware('auth')
+       ->name('admin.')
+       ->group( function(){
+           //rotte home admin
+           Route::get('/', 'HomeController@index')->name('home');
+       });
+
+
+ Route::get('{any?}', function () {
+    return view('guest.welcome');
+ })->where('any', '.*');
+
+
