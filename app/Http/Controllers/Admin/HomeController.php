7ec42febdb8eb215ce5dd5ef8ea20e5a5cc22bbc
MatteoLiberati <matteoliberati@hotmail.com>
2021-07-09 20:42:22 +0200

    section for user with restaurant

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -11,14 +11,16 @@ class HomeController extends Controller
 {
     public function index(){
         $auth_id = Auth::id();
-        $restaurants_db = Restaurant::all()->pluck("user_id");
-        $logged = false;
+        $restaurants_db = Restaurant::all()->pluck("user_id"); 
+        $my_restaurant = null; 
 
         foreach ($restaurants_db as $user_restaurant){
-            if($auth_id === $user_restaurant)
-            $logged = true;
+            if($auth_id === $user_restaurant){
+                $logged = true;
+                $my_restaurant = Restaurant::where("user_id",$auth_id)->get()->first();
+            }
         }
 
-        return view('admin.home', compact('auth_id', 'restaurants_db', 'logged'));
+        return view('admin.home', compact('auth_id', 'restaurants_db', 'my_restaurant'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,15 +3,37 @@
 @section('content')
 <div class="container">
 
-    
-    @if ($logged)
-        <h1>Your restaurant</h1>
+    {{-- IF USER HAS A RESTAURANT --}}
+    @if ($my_restaurant)
+        <div class="text-center">
+            <h1 class="my-5">{{$my_restaurant->name}}</h1>
 
+            <div class="d-flex">
+                {{-- INFORMATIONS --}}
+                <div class="informations w-50 d-flex flex-column justify-content-center">
+                    <h3>address: {{$my_restaurant->address}}</h3>
+                    <h4>phone number: {{$my_restaurant->phone_number}}</h4>
+                </div>
+                {{-- IMAGE --}}
+                <div class="w-50"><img class="img-fluid" src="{{$my_restaurant->image}}" alt="{{$my_restaurant->name}}"></div>
+            </div>
+
+            {{-- TIPOLOGIES --}}
+
+            {{-- EDIT --}}
+            <a class="btn btn-success m-4"href="">i tuoi prodotti</a>
+            <a class="btn btn-warning m-4"href="">Modifica dati</a>
+        </div>
+
+    {{-- IF USER DOESN'T HAVE A RESTAURANT --}}
     @else
-    <div>Welcome User</div>
-    <a class="btn btn-primary" 
-    href="{{route("admin.restaurant.create")}}"
-    >Register your restaurant</a>
+    <h1 class="text-center my-4">Benvenuto</h1>
+        <div class="text-center">
+            <h2 class="my-4">Registra subito la tua attività</h2>
+            <a class="btn btn-primary"
+            href="{{route("admin.restaurant.create")}}"
+            >Registra</a>
+        </div>
     @endif
 
 </div>
