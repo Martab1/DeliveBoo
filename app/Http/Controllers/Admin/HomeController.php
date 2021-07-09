564ed05905ab6a13847be9054622ecaa93fe7169
MatteoLiberati <matteoliberati@hotmail.com>
2021-07-09 19:55:23 +0200

    logged

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(){
-        
-        return view('admin.home');
+        $auth_id = Auth::id();
+        $restaurants_db = Restaurant::all()->pluck("user_id");
+        $logged = false;
+
+        foreach ($restaurants_db as $user_restaurant){
+            if($auth_id === $user_restaurant)
+            $logged = true;
+        }
+
+        return view('admin.home', compact('auth_id', 'restaurants_db', 'logged'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,9 +2,17 @@
 
 @section('content')
 <div class="container">
+
+    
+    @if ($logged)
+        <h1>Your restaurant</h1>
+
+    @else
     <div>Welcome User</div>
-    <a class="btn btn-primary " 
+    <a class="btn btn-primary" 
     href="{{route("admin.restaurant.create")}}"
     >Register your restaurant</a>
+    @endif
+
 </div>
 @endsection
