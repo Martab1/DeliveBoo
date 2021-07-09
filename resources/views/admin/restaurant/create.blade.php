816042828fbee5231750f89a78e1ff17ce0ae3ad
MatteoLiberati <matteoliberati@hotmail.com>
2021-07-09 16:08:04 +0200

    submit button

--- resources/views/admin/restaurant/create.blade.php
+++ resources/views/admin/restaurant/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h1 class="my-2">Crea il tuo ristorante</h1>
-        <form action="{{route('admin.restaurant.store')}}" method="post">
+        <form action="{{route('admin.restaurant.store')}}" method="post" enctype="multipart/form-data">
         @csrf
         @method("POST")
             <div class="form-group">
@@ -34,6 +34,11 @@
                 <label for="image"></label>
                 <input role="button" type="file" id="image" name="image">
             </div>
+            
+            <div class="form-group mx-2">
+                <input role="button" type="submit" class="btn btn-primary">
+            </div>
+
         </form>
     </div>
 @endsection
