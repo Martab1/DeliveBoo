c0c420cacb9b60e79d0f860d664e8ead3ccd5b3c
Martab1 <martabernardo86@gmail.com>
2021-07-12 10:22:16 +0200

    ready to merge

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Tipology;
+use App\Restaurant;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\support\Str;
 
 class RestaurantController extends Controller
 {
@@ -38,7 +42,41 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
+
+         $request->validate([
+            'name'=> 'required|max:50', 
+            'address' => 'required|max:255',
+            'phone_number' => 'required|min:10|max:15',
+            'tipologies' => 'required|exists:tipologies,id',
+            'image' => 'nullable|max:1000|mimes:jpg,jpeg,png,bmp',
+        ], [
+            'required'=> 'The :attribute is required!',
+            'unique'=> 'The :attribute is already is use for another post',
+            'max'=> 'Max :max characters allowed for the :attribute',
+
+        ]);
+
+
+         $data = $request->all();
+
+         //generate slug
+         $data['slug'] = Str::slug($data['name'], '-');
+
+         // user
+         $data['user_id'] = Auth::user()->id;
         
+        $new_restaurant = new Restaurant();
+        $new_restaurant->fill($data);
+        $new_restaurant->save();
+
+        // Save relation with tipologies (pivot)
+        if(array_key_exists('tipologies', $data)){
+            $new_restaurant->tipologies()->attach($data['tipologies']); // aggiunge i nuovi records in table pivot
+        }
+
+        return view('admin.home');
+
+
     }
 
     /**
@@ -47,11 +85,12 @@ class RestaurantController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Restaurant $restaurant)
+
     {
-        //
     }
 
+   
     /**
      * Show the form for editing the specified resource.
      *
