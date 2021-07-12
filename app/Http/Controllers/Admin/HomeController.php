7a6b44220a8fb3bdc4979e418c99aa32cebc64d4
MatteoLiberati <matteoliberati@hotmail.com>
2021-07-12 09:52:14 +0200

    merge

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -17,24 +17,11 @@ class HomeController extends Controller
 
         foreach ($restaurants_db as $user_restaurant){
             if($user_auth->id === $user_restaurant){
-                $logged = true;
                 $my_restaurant = Restaurant::where("user_id",$user_auth->id)->get()->first();
                 $my_tipologies = $my_restaurant->tipologies;
             }
         }
 
-        // POPOLAZIONE TABELLA PIVOT 
-        // $array1 = [1,5,2,4,8];
-        // $array2 = [2,4,2,5,8];
-
-        // $restaurant_2 = Restaurant::find(2);
-        // $restaurant_3 = Restaurant::find(3);
-        // $restaurant_2->tipologies()->attach($array1);
-        // $restaurant_3->tipologies()->attach($array2);
         return view('admin.home', compact('my_restaurant', 'user_auth', 'my_tipologies'));
     }
 }
-
-
-// CANCELLARE SEEDER FITTIZIO RESTAURANT
-// CANCELLARE POPOLAZIONE TABELLA PIVOT 
