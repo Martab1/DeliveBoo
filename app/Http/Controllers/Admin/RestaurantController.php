cf49b1d88ac10cd0d81e3afcf26198cdf12ffab6
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-07-12 10:57:24 +0200

    hope2

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Restaurant;
 use App\Tipology;
-use illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
