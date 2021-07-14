a6808455b03518502a43081920bc1b894c9bd69e
GiuseppeSoraci <giuseppesoraci@outlook.it>
2021-07-14 13:06:19 +0200

    giuseppe

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -11,17 +11,16 @@ class HomeController extends Controller
 {
     public function index(){
         $user_auth = Auth::user();
-        $restaurants_db = Restaurant::all()->pluck("user_id"); 
-        $my_restaurant = null;
-        $my_tipologies = []; 
+        $restaurants_db = Restaurant::all()->pluck("user_id");
+        $my_restaurants = null;
+
 
         foreach ($restaurants_db as $user_restaurant){
             if($user_auth->id === $user_restaurant){
-                $my_restaurant = Restaurant::where("user_id",$user_auth->id)->get()->first();
-                $my_tipologies = $my_restaurant->tipologies;
+                $my_restaurants = Restaurant::where("user_id",$user_auth->id)->get();
             }
         }
 
-        return view('admin.home',  compact('my_restaurant', 'user_auth', 'my_tipologies'));
+        return view('admin.home',  compact('my_restaurants', 'user_auth'));
     }
 }
