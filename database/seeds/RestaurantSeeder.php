cd27df77dc6e4b49a337083a370f0db173427db4
GiuseppeSoraci <giuseppesoraci@outlook.it>
2021-07-25 10:06:12 +0200

    bella raga

--- database/seeds/RestaurantSeeder.php
+++ database/seeds/RestaurantSeeder.php
@@ -12,5 +12,51 @@ class RestaurantSeeder extends Seeder
      */
     public function run()
     {
+        $restaurants = [
+
+            // Pizzerie
+            [
+                'name' => 'Al Vialetto',
+                'address' => 'Roma',
+            ],
+            [
+                'name' => 'Pizza Sprint',
+                'address' => 'Palermo',
+            ],
+            [
+                'name' => 'Verace',
+                'address' => 'Milano',
+            ],
+            [
+                'name' => 'Pino Pizza',
+                'address' => 'Torino',
+            ],
+            [
+                'name' => 'Pizza da Gino',
+                'address' => 'Genova',
+            ],
+            [
+                'name' => 'Vesuvio',
+                'address' => 'Napoli',
+            ],
+            [
+                'name' => 'Vesuvio',
+                'address' => 'Napoli',
+            ],
+            [
+                'name' => 'Vesuvio',
+                'address' => 'Napoli',
+            ],
+            [
+                'name' => 'Vesuvio',
+                'address' => 'Napoli',
+            ],
+            [
+                'name' => 'De Santis',
+                'address' => 'Foggia',
+            ],
+
+            // Giapponese
+        ];
     }
 }
