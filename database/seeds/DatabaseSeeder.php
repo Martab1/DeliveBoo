5a69450c23396e2fc1f6203f9300078bc6dbed6e
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-07-25 15:19:04 +0200

    alex

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TipologySeeder::class,
             CategorySeeder::class,
+            RestaurantSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
