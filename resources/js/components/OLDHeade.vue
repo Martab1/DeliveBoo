bc97be56df326d94a03b393b9a18b293f05fc3ab
MatteoLiberati <matteoliberati@hotmail.com>
2021-07-30 18:24:37 +0200

    ultimo cambiamento

--- resources/js/components/OLDHeade.vue
+++ resources/js/components/OLDHeade.vue
@@ -49,7 +49,7 @@
 
 <script>
 export default {
-   name: 'Header',
+  //  name: 'Header',
 }
 </script>
 
--- resources/js/pages/OLDRestaurantShow.vue
+++ resources/js/pages/OLDRestaurantShow.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="" >
-        <HeaderWhite/>
+        <!-- <HeaderWhite/> -->
         <!-- <div class="container flex">
             <SingleRestaurant
                 
@@ -68,161 +68,161 @@
     </div>
 </template>
 
-<script>
-import Cart from "../components/Cart.vue";
-import HeaderWhite from '../components/HeaderWhite.vue';
-import SingleRestaurant from "../components/SingleRestaurant.vue";
-export default {
+ <script>
+// import Cart from "../components/Cart.vue";
+// import HeaderWhite from '../components/HeaderWhite.vue';
+// import SingleRestaurant from "../components/SingleRestaurant.vue";
+// export default {
     // name: "RestaurantShow",
-    components: {
-        Cart,
-        SingleRestaurant,
-        HeaderWhite,
-    },
-    data() {
-        return {
-            my_restaurant: null,
-            my_categories: [],
-            cart: {
-                key: "",
-                products: []
-            },
-            total: 0
-        };
-    },
-    created() {
-        this.getRestaurant();
-    },
-    methods: {
-        getRestaurant() {
-            axios
-                .get(
-                    `http://127.0.0.1:8000/api/restaurants/${this.$route.params.slug}`
-                )
-                .then(res => {
-                    this.my_restaurant = res.data;
-                    this.cart.key = res.data.restaurant.id;
-                    res.data.products.forEach(element => {
-                        if (
-                            !this.my_categories.includes(element.category.name)
-                        ) {
-                            this.my_categories.push(element.category.name);
-                        }
-                    });
-                    this.init();
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        },
+    // components: {
+    //     Cart,
+    //     SingleRestaurant,
+    //     HeaderWhite,
+    // },
+    // data() {
+    //     return {
+    //         my_restaurant: null,
+    //         my_categories: [],
+    //         cart: {
+    //             key: "",
+    //             products: []
+    //         },
+    //         total: 0
+    //     };
+    // },
+    // created() {
+    //     this.getRestaurant();
+    // },
+    // methods: {
+    //     getRestaurant() {
+    //         axios
+    //             .get(
+    //                 `http://127.0.0.1:8000/api/restaurants/${this.$route.params.slug}`
+    //             )
+    //             .then(res => {
+    //                 this.my_restaurant = res.data;
+    //                 this.cart.key = res.data.restaurant.id;
+    //                 res.data.products.forEach(element => {
+    //                     if (
+    //                         !this.my_categories.includes(element.category.name)
+    //                     ) {
+    //                         this.my_categories.push(element.category.name);
+    //                     }
+    //                 });
+    //                 this.init();
+    //             })
+    //             .catch(err => {
+    //                 console.log(err);
+    //             });
+    //     },
 
         //AGGIUNTA PRODOTTI NELL'ARRAY CHE POPOLERA IL CART DAL LOCALSTORAGE
-        add(product) {
-            if (this.find(product.id)) {
-                this.increment(product.id);
-            } else {
-                let obj = {
-                    name: product.name,
-                    id: product.id,
-                    price: product.price,
-                    qty: 1,
-                    total: product.price
-                };
-                this.cart.products.push(obj);
-            }
-            this.total += product.price;
-            this.sync();
-        },
+        // add(product) {
+        //     if (this.find(product.id)) {
+        //         this.increment(product.id);
+        //     } else {
+        //         let obj = {
+        //             name: product.name,
+        //             id: product.id,
+        //             price: product.price,
+        //             qty: 1,
+        //             total: product.price
+        //         };
+        //         this.cart.products.push(obj);
+        //     }
+        //     this.total += product.price;
+        //     this.sync();
+        // },
 
         //RIMUOVI PRODOTTI
-        remove(product) {
-            if (this.find(product.id)) {
-                this.decrement(product.id);
-            }
-            this.sync();
-            if (this.total != 0) {
-                this.total -= product.price;
-            } else {
-                this.total = 0;
-            }
-        },
+        // remove(product) {
+        //     if (this.find(product.id)) {
+        //         this.decrement(product.id);
+        //     }
+        //     this.sync();
+        //     if (this.total != 0) {
+        //         this.total -= product.price;
+        //     } else {
+        //         this.total = 0;
+        //     }
+        // },
 
         //SINCRONIZZAZIONE DATI LOCAL STORAGE
-        async sync() {
-            let content = JSON.stringify(this.cart.products);
-            await localStorage.setItem(this.cart.key, content);
-        },
+        // async sync() {
+        //     let content = JSON.stringify(this.cart.products);
+        //     await localStorage.setItem(this.cart.key, content);
+        // },
 
         //CONTROLLO SE UN PRODOTTO è GIA PRESENTE NEL CARRELLO
-        find(id) {
-            let check = undefined;
-            this.cart.products.forEach(e => {
-                if (e.id == id) {
-                    check = true;
-                }
-            });
-            return check;
-        },
+        // find(id) {
+        //     let check = undefined;
+        //     this.cart.products.forEach(e => {
+        //         if (e.id == id) {
+        //             check = true;
+        //         }
+        //     });
+        //     return check;
+        // },
 
         //INCREMENTO QTY
-        increment(id) {
-            this.cart.products = this.cart.products.map(e => {
-                if (e.id == id) {
-                    e.qty += 1;
-                    e.total = e.price * e.qty;
-                    return e;
-                } else {
-                    return e;
-                }
-            });
-        },
+        // increment(id) {
+        //     this.cart.products = this.cart.products.map(e => {
+        //         if (e.id == id) {
+        //             e.qty += 1;
+        //             e.total = e.price * e.qty;
+        //             return e;
+        //         } else {
+        //             return e;
+        //         }
+        //     });
+        // },
 
         //DECREMENTO QTY
-        decrement(id) {
-            this.cart.products.forEach(e => {
-                if (e.id == id) {
-                    e.qty -= 1;
-                    e.total = e.price * e.qty;
-                    if (e.qty == 0) {
-                        this.cart.products.forEach((e, index) => {
-                            if (e.id == id) {
-                                this.cart.products.splice(index, 1);
-                            }
-                        });
-                    }
-                }
-            });
-        },
+        // decrement(id) {
+        //     this.cart.products.forEach(e => {
+        //         if (e.id == id) {
+        //             e.qty -= 1;
+        //             e.total = e.price * e.qty;
+        //             if (e.qty == 0) {
+        //                 this.cart.products.forEach((e, index) => {
+        //                     if (e.id == id) {
+        //                         this.cart.products.splice(index, 1);
+        //                     }
+        //                 });
+        //             }
+        //         }
+        //     });
+        // },
         // RIMUOVI TUTTO IL CARRELLO
-        removeCart(cart) {
-            if (cart != 0) {
-                this.total = 0;
-                this.cart.products = [];
-            }
-            this.sync();
-        },
-        init() {
-            for (var i = 0; i < localStorage.length; i++) {
-                if (this.cart.key != localStorage.key(i)) {
-                    localStorage.removeItem(i);
-                }
-            }
-            let contents = localStorage.getItem(this.cart.key);
-            if (contents) {
-                this.cart.products = JSON.parse(contents);
-            }
-            this.cart.products.forEach(e => {
-                this.total += e.total;
-            });
-        }
-    }
-};
+//         removeCart(cart) {
+//             if (cart != 0) {
+//                 this.total = 0;
+//                 this.cart.products = [];
+//             }
+//             this.sync();
+//         },
+//         init() {
+//             for (var i = 0; i < localStorage.length; i++) {
+//                 if (this.cart.key != localStorage.key(i)) {
+//                     localStorage.removeItem(i);
+//                 }
+//             }
+//             let contents = localStorage.getItem(this.cart.key);
+//             if (contents) {
+//                 this.cart.products = JSON.parse(contents);
+//             }
+//             this.cart.products.forEach(e => {
+//                 this.total += e.total;
+//             });
+//         }
+//     }
+// };
 </script>
 
 <style scoped lang="scss">
-@import '../../sass/vars.scss';
-@import '../../sass/general.scss';
-@import '../../sass/mixins.scss';
+// @import '../../sass/vars.scss';
+// @import '../../sass/general.scss';
+// @import '../../sass/mixins.scss';
 
 /* Carrello*/
 // .container{
