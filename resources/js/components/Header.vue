b42b27799ce614d28b0837e9b9cfc6a4c3843675
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-08-02 19:37:25 +0200

    header responsive

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -32,13 +32,14 @@
           </div>
         </span>
 
-        <div>
+        <div class="r-none1">
           <svg height="24" width="24" viewBox="0 0 24 24" class="ccl-0f24ac4b87ce1f67 ccl-ed34b65f78f16205 ccl-c738ab1fde928049"><path d="M3 10L5 8.44444V5H9.42857L12 3L21 10V21H3V10ZM14 19H19V10.9782L12 5.53372L5 10.9782V19H10V14H14V19Z"></path></svg>
-          Registrati o accedi</div>
-        <div>
+          Registrati o accedi
+        </div>
+        <div class="r-none2">
           <svg height="24" width="24" viewBox="0 0 24 24" class="ccl-0f24ac4b87ce1f67 ccl-ed34b65f78f16205 ccl-c738ab1fde928049"><path d="M2 13V11H22V13H2ZM2 19V17H22V19H2ZM2 7V5H22V7H2Z"></path></svg>
           Menu
-          </div>
+        </div>
       </div>
     </div>
   </header>
@@ -63,6 +64,31 @@ export default {
 <style scoped lang="scss">
 @import '../../sass/vars.scss';
 @import '../../sass/mixins.scss';
+
+//RESPONSIVE
+@media screen and (max-width: 1023px) {
+  header {
+    .my_container{
+      .actions{
+        .r-none1{
+          display:none;
+        }
+      }
+    }
+  }
+
+}
+@media screen and (max-width: 615px) {
+  header {
+    .my_container{
+      .actions{
+        .r-none2{
+          display:none;
+        }
+      }
+    }
+  }
+}
 
 // UTILITIES
 .relative{
